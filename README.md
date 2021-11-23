8748c1dd834202a0d4177c19231c25c7c29a5a3d
FunnySkillz <sebastijan.bogdan@outlook.com>
2021-11-23 16:36:16 +0100

    update README.md gh pages

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 github repo: https://github.com/htl-leonding-project/2022-4ahif-leoquest.git
 
-github pages: 
+github pages: https://htl-leonding-project.github.io/2022-4ahif-leoquest
 
 Members: Abazovic Edina  (PL), Bogdan Sebastijan, 
 Binder Paul, Plakolb Marcel
--- publish-gh-pages-inside-docker.sh
+++ publish-gh-pages-inside-docker.sh
@@ -45,3 +45,5 @@ echo "***********************************************************"
 # https://stackoverflow.com/a/918931
 # maybe better with subtrees -> https://gist.github.com/cobyism/4730490
 # https://blog.bloomca.me/2017/12/15/how-to-push-folder-to-github-pages.html
+
+
