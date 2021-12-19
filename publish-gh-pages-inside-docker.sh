74205f926f9b6d0f50a81b36f051315842a54f11
FunnySkillz <sebastijan.bogdan@outlook.com>
2021-12-19 21:00:53 +0100

    update scripts for gh pages

--- publish-gh-pages-inside-docker.sh
+++ publish-gh-pages-inside-docker.sh
@@ -22,7 +22,19 @@ git config --global user.email ${GIT_GLOBAL_MAIL}
 git config --global user.name ${GIT_GLOBAL_USER_NAME}
 git init
 git add .
-git commit -m "$USER - rebuilding gh-pages $(date)"
+git commit -m "$GIT_GLOBAL_USER_NAME - rebuilding gh-pages $(date)"
+
+# rename branch to 'main' if necessary
+CURRENT_BRANCH=$(git branch --show-current)
+echo "current branch: $CURRENT_BRANCH"
+if [ "$CURRENT_BRANCH" != "main" ]
+then
+  git branch -M main
+  echo "branch $CURRENT_BRANCH renamed to 'main'"
+else
+  echo "branch $CURRENT_BRANCH not renamed"
+fi
+
 git remote add origin $ORIGIN_URL
 
 
@@ -45,5 +57,3 @@ echo "***********************************************************"
 # https://stackoverflow.com/a/918931
 # maybe better with subtrees -> https://gist.github.com/cobyism/4730490
 # https://blog.bloomca.me/2017/12/15/how-to-push-folder-to-github-pages.html
-
-
--- run-inside-docker.sh
+++ run-inside-docker.sh
@@ -1,13 +1,12 @@
 set -e
 BUILD_DIR="gh-pages"
 echo "BUILD_DIR->${BUILD_DIR}"
-[ -d $BUILD_DIR ] && rm -r $BUILD_DIR  # remove dir, if existing
 OLD=${PWD}
 echo "OLD->${OLD}"
 docker run --rm \
            -v ${PWD}:/documents \
            --env-file ./.env \
-           asciidoctor/docker-asciidoctor:1.11.0  /bin/bash -c "/documents/publish-gh-pages-inside-docker.sh"
+           asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/publish-gh-pages-inside-docker.sh"
            #asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/build-html-docker-inside-docker.sh"
 cd $PWD/$BUILD_DIR
 CURRENT_FOLDER=${PWD}
