eff3da40e9a160a6fb1517b3d820ca2787a9508e
FunnySkillz <sebastijan.bogdan@outlook.com>
2022-02-01 14:51:24 +0100

    add entity question and questionType

---
+++ src/main/java/at/htl/entity/Question.java
@@ -0,0 +1,102 @@
+package at.htl.entity;
+
+import at.htl.entity.Questionnaire;
+import com.sun.xml.bind.v2.TODO;
+
+import javax.persistence.*;
+import java.sql.Blob;
+
+@NamedQueries({
+
+        @NamedQuery(
+                name = "Question.findAll",
+                query = "select q from Question q"
+        )
+
+})
+@Entity
+@Table(name = "Question")
+public class Question {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "q_id")
+    private Long id;
+    @Column(name = "q_text")
+    private String text;
+    @Column(name = "q_seqNumber")
+    private int seqNumber;
+
+    //TODO change to lookup table
+    @ManyToOne
+    @JoinColumn(name = "q_type")
+    private QuestionType type;
+
+    @ManyToOne
+    @JoinColumn(name = "q_questionnaire")
+    private Questionnaire questionnaire;
+
+    public Question() {
+
+    }
+
+    public Question(String text, int seqNumber, QuestionType type, Questionnaire questionnaire) {
+        this.text = text;
+        this.seqNumber = seqNumber;
+
+        this.type = type;
+        this.questionnaire = questionnaire;
+    }
+
+    public Question(Long id, String text, Blob image, int seqNumber, QuestionType type, Questionnaire questionnaire) {
+        this.id = id;
+        this.text = text;
+
+        this.seqNumber = seqNumber;
+        this.type = type;
+        this.questionnaire = questionnaire;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long q_id) {
+        this.id = q_id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String q_text) {
+        this.text = q_text;
+    }
+
+    public int getSeqNumber() {
+        return seqNumber;
+    }
+
+    public void setSeqNumber(int q_sequenceNumber) {
+        this.seqNumber = q_sequenceNumber;
+    }
+
+
+    public Questionnaire getQuestionnaire() {
+        return questionnaire;
+    }
+
+    public void setQuestionnaire(Questionnaire q_questionnaire) {
+        this.questionnaire = q_questionnaire;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", sequenceNumber=" + seqNumber +
+                ", type=" + type +
+                ", questionnaire=" + questionnaire +
+                '}';
+    }
+}
---
+++ src/main/java/at/htl/entity/QuestionType.java
@@ -0,0 +1,29 @@
+package at.htl.entity;
+
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Table(name = "QuestionType")
+public class QuestionType {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "qt_id")
+    Long id;
+
+    @Column(name = "qt_name")
+    String name;
+
+    //TODO JPA map
+
+
+    public QuestionType() {
+    }
+
+    public QuestionType(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
