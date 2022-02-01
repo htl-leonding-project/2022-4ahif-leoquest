a3461d7c4d2e1894f6dbf51e0f1860631262b162
FunnySkillz <sebastijan.bogdan@outlook.com>
2022-02-01 15:09:05 +0100

    add hashmap for QuestionType

---
+++ src/main/java/at/htl/entity/ChosenOption.java
@@ -0,0 +1,61 @@
+package at.htl.entity;
+
+import at.htl.entity.Answer;
+import at.htl.entity.AnswerOption;
+
+import javax.persistence.*;
+
+@NamedQueries({
+
+        @NamedQuery(
+                name = "ChosenOption.findAll",
+                query = "select co from ChosenOption co"
+        )
+
+})
+@Entity
+@Table(name = "ChosenOption")
+public class ChosenOption {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "co_answerOption")
+    private AnswerOption answerOption;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "co_answer")
+    private Answer answer;
+
+    public ChosenOption() {
+    }
+
+    public ChosenOption(AnswerOption answerOption, Answer answer) {
+        this.answerOption = answerOption;
+        this.answer = answer;
+    }
+
+    public AnswerOption getAnswerOption() {
+        return answerOption;
+    }
+
+    public void setAnswerOption(AnswerOption co_answerOption) {
+        this.answerOption = co_answerOption;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer co_answer) {
+        this.answer = co_answer;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ChosenOption{" +
+                "answerOption=" + answerOption +
+                ", answer=" + answer +
+                '}';
+    }
+}
