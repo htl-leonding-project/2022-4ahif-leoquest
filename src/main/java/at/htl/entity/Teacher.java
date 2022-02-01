caa1f8560efe2eb26303293ba1fcf4dc1c420ca2
FunnySkillz <sebastijan.bogdan@outlook.com>
2022-02-01 14:27:34 +0100

    run in docker change user and pw

---
+++ src/main/java/at/htl/entity/Teacher.java
@@ -0,0 +1,62 @@
+package at.htl.entity;
+
+import javax.persistence.*;
+
+
+@NamedQueries({
+
+        @NamedQuery(
+                name = "Teacher.findAll",
+                query = "select t from Teacher t order by t.name"
+        )
+
+})
+
+
+@Entity
+@Table(name = "Teacher")
+public class Teacher {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "t_id")
+    private Long id;
+    @Column(name = "t_name")
+    private String name;
+
+    public Teacher() {
+    }
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public Teacher(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long t_id) {
+        this.id = t_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String t_name) {
+        this.name = t_name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
