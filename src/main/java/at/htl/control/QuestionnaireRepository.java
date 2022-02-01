350605d05562d11ebecab4c949acae47d39ef6b2
AbazovicEdinaIF <edina15042305@gmail.com>
2022-02-01 16:52:13 +0100

    modify QuestionnaireRepository

--- src/main/java/at/htl/control/QuestionnaireRepository.java
+++ src/main/java/at/htl/control/QuestionnaireRepository.java
@@ -1,4 +1,30 @@
 package at.htl.control;
 
+import at.htl.entity.Questionnaire;
+import at.htl.entity.Teacher;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public class QuestionnaireRepository {
+    @Inject
+    EntityManager em;
+
+    @Transactional
+    public void delete(Questionnaire questionnaire) {
+        em.remove(questionnaire);
+    }
+
+    @Transactional
+    public void save(Questionnaire questionnaire){
+        em.merge(questionnaire);
+    }
+
+    public List<Questionnaire> findAll() {
+        return em
+                .createNamedQuery("Questionnaire.findAll", Questionnaire.class)
+                .getResultList();
+    }
 }
