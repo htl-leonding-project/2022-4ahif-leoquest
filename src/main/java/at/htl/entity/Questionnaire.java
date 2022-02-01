566c12395d8fe85c16f05502a05922580447fdf8
FunnySkillz <sebastijan.bogdan@outlook.com>
2022-02-01 14:33:46 +0100

    add entity Questionnaire

---
+++ src/main/java/at/htl/entity/Questionnaire.java
@@ -0,0 +1,71 @@
+package at.htl.entity;
+
+import javax.persistence.*;
+
+@NamedQueries({
+
+        @NamedQuery(
+                name = "Questionnaire.findAll",
+                query = "select qn from Questionnaire qn"
+        )
+
+})
+@Entity
+@Table(name = "Questionnaire")
+public class Questionnaire {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "qn_id")
+    private Long id;
+    @Column(name = "qn_name")
+    private String name;
+    @Column(name = "qn_desc")
+    private String desc;
+
+    public Questionnaire() {
+    }
+
+    public Questionnaire(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public Questionnaire(Long id, String name, String desc) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String qn_name) {
+        this.name = qn_name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String qn_desc) {
+        this.desc = qn_desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Questionnaire{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+}
