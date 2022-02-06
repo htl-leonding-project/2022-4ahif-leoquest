7679273dba0faca2dea6cb85e6df816eda729afd
AbazovicEdinaIF <edina15042305@gmail.com>
2022-02-06 17:09:30 +0100

    add imports to import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+insert into ld_teacher (t_name)
+values ('Parmenter'),
+       ('Baker'),
+       ('Hope'),
+       ('Everett'),
+       ('Collins'),
+       ('Moorman'),
+       ('Wise'),
+       ('Newcastle'),
+       ('Parmenter'),
+       ('Collins'),
+       ('Miller');
