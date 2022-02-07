8d2f7d1844e843cce4a5444b77927585519ebff4
FunnySkillz <sebastijan.bogdan@outlook.com>
2022-02-07 21:37:19 +0100

    add get and setter

--- src/main/java/at/htl/entity/QuestionType.java
+++ src/main/java/at/htl/entity/QuestionType.java
@@ -24,6 +24,22 @@ public class QuestionType {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public QuestionType(Long id, String name) {
         this.id = id;
         this.name = name;
