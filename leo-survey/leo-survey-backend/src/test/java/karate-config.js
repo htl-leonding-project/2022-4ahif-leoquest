515707ac3c322e715dea4bbcdc19ff15ad191cd4
FunnySkillz <sebastijan.bogdan@outlook.com>
2022-06-20 09:31:15 +0200

    delete the demo karate tests

--- leo-survey/leo-survey-backend/src/test/java/karate-config.js
+++ leo-survey/leo-survey-backend/src/test/java/karate-config.js
@@ -5,7 +5,7 @@ function fn() {
         env = 'dev'; // a custom 'intelligent' default
     }
     var config = { // base config JSON
-        baseUrl: 'http://localhost:8080/api'
+        baseUrl: 'http://localhost:8081/api'
     };
     // don't waste time waiting for a connection or if servers don't respond within 5 seconds
     karate.configure('connectTimeout', 5000);
